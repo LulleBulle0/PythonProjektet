5ca40b8cc3e324c8b01795ec1deb63d4b37ef47b
Ponti07 <pontus.angrimer@skola.taby.se>
2024-12-02 22:44:00 +0100

    Lagt till klasser: Player, Trap och Monster

--- testspel.py
+++ testspel.py
@@ -1,127 +1,9 @@
-# print("[1]Spela!")
-# print("[2]Avbryt")
-# start = int(input("Är du redo för att spela? "))
-
-# import random
-
-# slumptal = random.randint(1, 3)
-# monster = random.randint(1, 100)
-# tressure = random.randint(1, 100)
-
-# def Troll():
-#     print("Monster!")
-#     print(monster)
-
-#     if monster <= 10:
-#         print()
-
-# def kista():
-#     print("Tressure!")
-#     print(tressure)
-#     if tressure <= 15:
-#         inventory.append("sheild")
-                
-#     elif 15 < tressure <= 30:
-#         inventory.append("healthpotion")
-                
-#     elif tressure > 30: 
-#          inventory.append("glass")
-
-# def fälla():
-#     print("Trap!")
-#     print(HP - 20)
-
-# jHP = 40
-# tHP = 30
-# sHP = 15
-
-
-# if start == 1:
-#     print("Spelet startas!")
-    
-#     inventory = []
-
-#     print("[1] Lätt")
-#     print("[2] Medel")
-#     print("[3] Svårt")
-#     Svårhetsgrad = int(input("Välj din svårhetsgrad "))
-
-#     if Svårhetsgrad == 1:
-#         HP = 100
-#         print("---Ditt HP---")
-#         print(HP)
-#         inventory.append("Svärd")
-#     elif Svårhetsgrad == 2:
-#         HP = 75
-#         print("---Ditt HP---")
-#         print(HP)
-#         inventory.append("svärd")
-#     elif Svårhetsgrad == 3:
-#         HP = 50
-#         print("---Ditt HP---")
-#         print(HP)
-#         inventory.append("svärd")
-    
-#     else:
-#         print("Ogiltligt svar!")
-
-#     while True:
-#         print("---MENY---")
-#         print("[1] Inventory")
-#         print("[2] Gå till första dörrarna")
-#         val = int(input("Vad vill du göra? "))
-
-#         if val == 1:
-            
-#             print(F"""
-#         Ditt inventory:
-#         {inventory}
-#             """)
-#             #Om man väljer inventory, så ska man sedan kunna gå vidare till dörrarna
-            
-
-#         elif val == 2:
-#             print("\nDu har kommit fram till dem första dörrarna!")
-#             input("Vill du gå in i dörr 1, 2 eller 3?")
-            
-
-    
-#             events = ["monster", "kista", "fälla"]
-                
-#             roomChoice = input("> ")
-                
-#             if(roomChoice in ["1", "2", "3"]):
-#                 event = random.choice(events)
-
-#                 if (event == "monster"):
-#                     Troll()
-#                 elif (event == "kista"):
-#                     kista()
-#                 elif (event == "fälla"):
-#                     fälla()
-
-    
-
-
-
-# elif start == 2:
-#     print("Du har valt att avbryta systemet")
-
-# else:
-#     print("ogiltligt svar")
-
-
-
-
-
 import random
 
 class Player:
     def __init__(self, difficulty):
         self.HP = {"1": 100, "2": 75, "3": 50}.get(difficulty, 0)
         self.inventory = ["Svärd"] if self.HP else []
-        
-
     
     def add_item(self, item):
         self.inventory.append(item)
@@ -148,7 +30,7 @@ class Game:
     
     def main_menu(self):
         while self.player.HP > 0:
-            choice = input("Vad vill du göra? \n[1] Inventory\n[2] Dörrar\n[3] Se din styrka\n> ")
+            choice = input("Vad vill du göra? \n[1] Inventory\n[2] Dörrar\n[3] Se dina egenskaper\n> ")
             if choice == "1":
                 print(f"Ditt inventory: {self.player.inventory}")
             elif choice == "2":
@@ -185,5 +67,146 @@ class Game:
     def trap_event(self):
         self.player.take_damage(20)
 
-# Starta spelet
-Game().start()
+# # ---------------------------------------------------
+
+# class Item:
+#     item_types = [("Sköld", 15), ("Hälsodryck", 10), ("Svärd", 20)] # "Glass" skulle kunna ge HP
+
+#     def __init__(self, name = "", strength = 0):
+#         self.name = name 
+#         self.strength = strength
+        
+#     def randomize(self):        
+#         random_item = random.choice(self.item_types)
+#         self.name = random_item[0]
+#         self.strength = random_item[1]
+#         return self
+
+#     def to_str(self):
+#         return f"{self.name}\t{self.strength}"
+    
+# class Inventory: 
+#     inventory = []
+    
+#     def add_item(self, item): 
+#         if len(self.inventory) < 5: 
+#             self.inventory.append(item)
+#         else: 
+#             if (isinstance(item, Item)):
+#                 print(f"Du hittade {item.name} men din ryggsäck är full!")
+#                 self.show()
+#                 svar = int(input("Ange siffra för att ersätta eller 0 för att skippa: "))
+#                 if svar < 6 and svar > 0: 
+#                     self.replace_item(svar - 1, item)
+                 
+#     def replace_item(self, item_index, item):
+#         if item_index >= len(self.inventory): 
+#             print("Den platsen finns inte!")
+#         else:
+#             self.inventory[item_index] = item
+
+#     def show(self): 
+#         index = 1
+#         for item in self.inventory:
+#             if isinstance(item, Item):
+#                 print(f"{index}. {item.to_str()}") 
+#                 index += 1
+
+# class Chest: 
+#     def __init__(self): 
+#         self.item = Item().randomize
+#         if (isinstance(self.item, Item)):
+#             print(self.item.to_str())
+    
+# item = Item().randomize()
+# print(f"Pryl = {item.name}, styrka = {item.strength}")
+
+# inventory = Inventory()
+# inventory.add_item(Item().randomize())
+# inventory.add_item(Item().randomize())
+# inventory.add_item(Item().randomize())
+# inventory.add_item(Item().randomize())
+# inventory.add_item(Item().randomize())
+# inventory.add_item(Item().randomize())
+# inventory.show()
+
+# chest = Chest()
+# if (isinstance(chest.item, Item)):
+#     print(chest.item.to_str())
+
+#-----------------------------------------------------------------------------------
+
+
+# Game().start()
+
+class Item:
+    item_types = [("Sköld", 15), ("Hälsodryck", 10), ("Svärd", 20)] # "Glass" skulle kunna ge HP
+
+    def __init__(self):
+        random_item = random.choice(self.item_types)
+        self.name = random_item[0]
+        self.strength = random_item[1]
+
+    def show(self):
+        print(f"{self.name}\t{self.strength}")
+    
+class Inventory: 
+    inventory = []
+    
+    def add_item(self, item): 
+        if len(self.inventory) < 5: 
+            # ryggsäck inte full, lägg till pryl
+            self.inventory.append(item)
+        else: 
+            # ryggsäck full, ersätta pryl?
+            if (isinstance(item, Item)):
+                print(f"Du hittade {item.name} men din ryggsäck är full!")
+                self.show()
+                svar = int(input("\nErsätt pryl i ryggsäck genom att ange position eller 0 för att skippa: "))
+                if svar < 6 and svar > 0: 
+                    self.replace_item(svar - 1, item)
+                    self.show()
+                else: 
+                    print("Inget ersatt i ryggsäcken!")
+                 
+    def replace_item(self, item_index, item):
+        if item_index >= len(self.inventory): 
+            print("Den platsen finns inte!")
+        else:
+            self.inventory[item_index] = item
+
+    def show(self): 
+        index = 0
+        print("\nRYGGSÄCK:")
+        print("---------")
+        for item in self.inventory:
+            if isinstance(item, Item):
+                print(f"{index + 1}. {item.name}\t{item.strength}") 
+                index += 1
+
+class Chest: 
+    def __init__(self): 
+        self.item = Item()
+    
+print("Test av slumpad pryl (ITEM)")
+print("---------------------------")
+item = Item()
+print(f"Pryl = {item.name}, styrka = {item.strength}")
+
+print("\nTest av ryggsäck (INVENTORY) med fler än 5 slumpade prylar (ITEMs)")
+print("------------------------------------------------------------------")
+inventory = Inventory()
+inventory.add_item(Item())
+inventory.add_item(Item())
+inventory.add_item(Item())
+inventory.add_item(Item())
+inventory.add_item(Item())
+inventory.add_item(Item())
+
+print("\nTest av kista (CHEST) med slumpad pryl (ITEM)")
+print("---------------------------------------------")
+chest = Chest()
+if (isinstance(chest.item, Item)):
+    chest.item.show()
+
+#-----------------------------------------------------------------------------------
